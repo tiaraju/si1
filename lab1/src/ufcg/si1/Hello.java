33108971359a2986fd3eaa9dc900d965f10a69e8
tiaraju <tiaraju.smaneoto@ccc.ufcg.edu.br>
2013-06-05 17:09:53 -0300

    codigo do lab1

---
+++ lab1/src/ufcg/si1/Hello.java
@@ -0,0 +1,14 @@
+package ufcg.si1;
+
+public class Hello {
+
+	
+	public String xpto(){
+		return "Lab 01 de Sistema de Informações 1";
+	}
+	
+	public static void main(String[] args) {
+		Hello hello = new Hello();
+		System.out.println(hello.xpto());
+	}
+}
---
+++ lab1/src/ufcg/si1/HelloTest.java
@@ -0,0 +1,21 @@
+package ufcg.si1;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class HelloTest {
+	Hello hello;
+	
+	@Before
+	public void Initialize(){
+		hello = new Hello();
+	}
+	
+	@Test
+	public void TestaMetodoXpto(){
+		//Assert.assertEquals("Lab 01 de Sistema de Informações 1",hello.xpto());
+	}
+
+}
